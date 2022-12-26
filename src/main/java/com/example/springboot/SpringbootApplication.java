42b56166e4e88d2b8181b1970ccf4ccabbe419fb
avidere <56676069+avidere@users.noreply.github.com>
2022-12-26 23:49:31 +0530

    Removed servlet
    Removed servlet

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication extends SpringBootServletInitializer {
+public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
